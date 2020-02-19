0ead4c1113f0f82558d42bb8fc3c14c5c1a04ecc
Brandon Lei <brandonlei135@gmail.com>
2020-02-19 03:48:50 -0800

    CreatedIceBullet
    Created icebullet prefab. Tried to create a moveable AND damageable patroller, changed enemyhealth to include patrol in it but had some difficulties. need to have access to enemy's move speed for ice bullet to properly work.

--- Platformer/Assets/Scripts/EnemyHealth.cs
+++ Platformer/Assets/Scripts/EnemyHealth.cs
@@ -5,15 +5,51 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     SpriteRenderer m_SpriteRenderer;
-    Color m_NewColor;
 
     public int health = 100;
+    public float moveSpeed; 
     private bool onFire = false;
+    private bool slowed = false;
     //public GameObject deathEffect;
+    [Header("Agent's patrol areas")]
+    public List<Transform> patrolLocations; //List of all the Transform locations the gameObject will patrol
+
+    [Space, Header("Agent")]
+    public GameObject patrollingGameObject; //Unity GameObject that patrols
+    private int nextPatrolLocation; //Keeps track of the patrol location
+
     void Start()
     {
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    void Update()
+    {
+        PatrolArea();
+    }
+
+    private void PatrolArea()
+    {
+        Flip();
+        patrollingGameObject.transform.position = Vector2.MoveTowards(patrollingGameObject.transform.position,
+            patrolLocations[nextPatrolLocation].position, moveSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(patrollingGameObject.transform.position, patrolLocations[nextPatrolLocation].position) <= 2)
+        {
+            nextPatrolLocation = (nextPatrolLocation + 1) % patrolLocations.Count; //Prevents IndexOutofBound by looping back through list
+        }
+    }
+
+    private void Flip()
+    {
+        Vector2 localScale = patrollingGameObject.transform.localScale;
+        if (patrollingGameObject.transform.position.x - patrolLocations[nextPatrolLocation].position.x > 0)
+            localScale.x = 1;
+        else
+            localScale.x = -1;
+        patrollingGameObject.transform.localScale = localScale;
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -29,7 +65,15 @@ public class EnemyHealth : MonoBehaviour
         {
             StartCoroutine(DamageOverTimeCoroutine(damage, damageTime));
         }
-        
+    }
+
+    public void slow(int slowAmount, int slowTime)
+    {
+        if (slowed == false)
+        {
+            StartCoroutine(SlowCoroutine(slowAmount, slowTime));
+        }
+            
     }
 
     IEnumerator DamageOverTimeCoroutine(int damageAmount, int duration)
@@ -47,11 +91,31 @@ public class EnemyHealth : MonoBehaviour
             yield return new WaitForSeconds(.5f);
         }
         onFire = false;
-        
+    }
+
+    IEnumerator SlowCoroutine(int slowAmount, int slowTime)
+    {
+        Color frozenColor = new Color(0.8f, 1.0f, 1.0f, .4f);
+        slowed = true;
+        if (moveSpeed > slowAmount)
+            moveSpeed -= slowAmount;
+        int timeSlowed = 0;
+        while(timeSlowed < slowTime)
+        {
+            m_SpriteRenderer.color = frozenColor;
+            timeSlowed += 1;
+            yield return new WaitForSeconds(.5f);
+            m_SpriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(.5f);
+        }
+        moveSpeed += slowAmount;
+        slowed = false;
     }
     void Die()
     {
         //Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
+
+
 }
