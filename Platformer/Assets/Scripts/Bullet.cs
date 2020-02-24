02e8893750f0accbf08c0ee5d543037f2d35eece
damianrivas <drivas1993@gmail.com>
2020-02-24 02:09:03 -0800

    Create FireEnemy prefab. Create Enemy tag. Modify FireBullet script. - Created the FireEnemy prefab. - FireEnemy uses the FireBullet prefab by default. - FireBullet script now propels the bullet to the player's location at the moment it spawns and continues in that direction until collision. - Adjusted values in the FireBullet prefab - Created a new tag called "Enemy" - Added the Enemy tag to all current enemy prefabs

--- Platformer/Assets/Scripts/Bullet.cs
+++ Platformer/Assets/Scripts/Bullet.cs
@@ -15,11 +15,14 @@ public class Bullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        EnemyHealth enemy = hitInfo.GetComponent<EnemyHealth>();
-        if(enemy != null)
+        if (hitInfo.CompareTag("Enemy"))
         {
-            enemy.TakeDamage(damage);
+            EnemyHealth enemy = hitInfo.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
     }
 }
--- Platformer/Assets/Scripts/EnemyProjectile.cs
+++ Platformer/Assets/Scripts/EnemyProjectile.cs
@@ -7,42 +7,37 @@ public class EnemyProjectile : MonoBehaviour
 
     public float speed;
 
-    private Transform player; 
-    private Vector2 target; 
+    private Transform player;
+    private Vector2 target;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform; 
-
-	target = new Vector2(player.position.x, player.position.y); 
-
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime); 
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
 
-
-	if(transform.position.x == target.x && transform.position.y == target.y) {
-	    DestroyProjectile(); 
-
-
-	}
-
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
     }
 
-
-    void OnTriggerEnter2D(Collider2D other){
-	if(other.CompareTag("Player")) {
-	    DestroyProjectile();
-	}
-
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
     }
 
-    void DestroyProjectile() {
-	Destroy(gameObject); 
-
-    } 
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
 }
