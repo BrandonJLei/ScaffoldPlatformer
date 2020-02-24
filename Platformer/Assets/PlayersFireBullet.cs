5c262e42a0629cebbcda718b3b6270bfaa3e9f86
Brandon Lei <brandonlei135@gmail.com>
2020-02-24 03:22:33 -0800

    Fixed firebullet prefab conflict
    Firebullet prefab was changed for enemy use,  so PlayersFireBullet prefab was created to fix the missing prefab.

---
+++ Platformer/Assets/PlayersFireBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersFireBullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 20;
+    public int damageDealtOverTime = 10;
+    public int damageTime = 5;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemyHealth enemy = hitInfo.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            enemy.DamageOverTime(damageDealtOverTime, damageTime);
+        }
+        Destroy(gameObject);
+    }
+}
