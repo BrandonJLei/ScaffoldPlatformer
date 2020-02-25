30279342d3677fdca5e9f8420c9b81cfe95455e7
atakahashi19 <39812344+atakahashi19@users.noreply.github.com>
2020-02-24 16:19:24 -0800

    Damaging patroller collision

---
+++ Platformer/Assets/Scripts/EnemyDamageCollision.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamageCollision : MonoBehaviour
+{
+
+
+    public float damage = 20;
+    //public Rigidbody2D rb;
+
+ void OnTriggerEnter2D(Collider2D col)
+         {
+             if (col.CompareTag("Player"))
+             {
+                PlayerHealthCollision player = col.GetComponent<PlayerHealthCollision>();
+                if (player != null) 
+		{
+			player.TakeDamage(damage);
+		}
+		Destroy(gameObject);
+		
+             }
+         }
+
+
+//	void OnCollisionEnter(Collision col)
+//	{
+//        if (col.transform.tag == "Player")
+//        {
+//            PlayerHealthCollision player = col.transform.GetComponent<PlayerHealthCollision>();
+//            if (player != null)
+//            {
+//                player.TakeDamage(damage);
+//            }
+//            Destroy(gameObject);
+//        }
+//
+//	}
+
+
+}
