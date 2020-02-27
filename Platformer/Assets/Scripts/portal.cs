be2465c8ee03a30fe27aad126a074c3a3601687b
roykonan <roykonan@gmail.com>
2020-02-26 20:09:31 -0800

    Two levels with portal

---
+++ Platformer/Assets/Scripts/portal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+Using UnityEngine.SceneManagement;
+
+public class portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(collider2D other)
+    {
+	if (other.CompareTag("Player"))
+		{
+			// put level name where one is
+			SceneManager.LoadScrene(1) 
+		}	    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
