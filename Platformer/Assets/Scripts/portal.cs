e3539df5c286453f292ca541b3511c09bee38ad7
roykonan <roykonan@gmail.com>
2020-03-04 02:40:10 -0800

    added portal and enemies to fire level

---
+++ Platformer/Assets/Scripts/portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portal : MonoBehaviour
+{   
+    public string levelName;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+        }
+    }
+    
+}
