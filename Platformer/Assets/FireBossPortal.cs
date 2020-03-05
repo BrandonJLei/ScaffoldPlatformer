5a941f1dcd4061c379a647dc7cc280623c488771
Victoria W <56736494+Tori-W@users.noreply.github.com>
2020-03-04 21:26:06 -0800

    FireBoss Portal
    - Portal appears after FireBoss is defeated

---
+++ Platformer/Assets/FireBossPortal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBossPortal : MonoBehaviour
+{
+    public GameObject fireBossPortal;
+    public GameObject fireBoss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireBossPortal.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        fireBossPortal.SetActive(true);
+    }
+}
