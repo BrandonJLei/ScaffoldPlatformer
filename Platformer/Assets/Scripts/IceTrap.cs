46ce4cf0bfd9a52f544e258bdfc568d84a0c4d16
damianrivas <drivas1993@gmail.com>
2020-03-07 17:37:08 -0800

    Add ice trap

--- Platformer/Assets/Scripts/IceTrap.cs
+++ Platformer/Assets/Scripts/IceTrap.cs
@@ -5,27 +5,66 @@ using UnityEngine;
 public class IceTrap : MonoBehaviour
 {
 
-    public int damage = 10;
+    [SerializeField]
+    private LayerMask GroundLayer;
+    [SerializeField]
+    private Transform GroundCheck;
+    public float damage = 20.0f;
+    public bool isActive = true;
+    public float despawnTime = 1.5f;
+
+    private bool hitGround;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (isActive)
+            if (collision.CompareTag("Player"))
+            {
+                rb.isKinematic = false;
+            }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isActive)
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                PlayerHealthCollision player = collision.gameObject.GetComponent<PlayerHealthCollision>();
+                if (player != null)
+                    player.TakeDamage(damage);
+            }
+    }
+
+    void FixedUpdate()
+    {
+        bool grounded = Physics2D.Linecast(transform.position, GroundCheck.position, GroundLayer);
+        if (grounded && isActive)
         {
-            PlayerHealthCollision player = collision.GetComponent<PlayerHealthCollision>();
-            if (player != null)
-                player.TakeDamage(damage);
+            isActive = false;
+            hitGround = true;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hitGround && !isActive)
+            despawn();
+    }
+
+    void despawn()
+    {
+        if (despawnTime <= 0)
+            Destroy(gameObject);
+        else
+            despawnTime -= Time.deltaTime;
     }
 }
