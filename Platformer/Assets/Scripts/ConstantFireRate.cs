287c073fdad91adc45a8c3579b8d064d5f760d3a
damianrivas <drivas1993@gmail.com>
2020-03-08 21:51:22 -0700

    ConstantFireRate now only spawns bullets if Player is in range

--- Platformer/Assets/Scripts/ConstantFireRate.cs
+++ Platformer/Assets/Scripts/ConstantFireRate.cs
@@ -5,32 +5,46 @@ using UnityEngine;
 public class ConstantFireRate : MonoBehaviour
 {
 
-    private float timeBtwShots; 
+    private float timeBtwShots;
     public float startTimeBtwShots;
     public GameObject projectile;
-	public Transform enemyFirePoint;
+    public Transform enemyFirePoint;
+    [SerializeField, Range(0, 2.0f)]
+    public float cameraDistanceMultiplier = 0.75f;
 
-	void Start()
-	{
-		timeBtwShots = startTimeBtwShots;
-	}
+    private float fireDistance;
 
-
-	void Update() 
-	{
-
-	if (timeBtwShots <= 0) {
-	    Instantiate(projectile, enemyFirePoint.position, Quaternion.identity); 
-	    timeBtwShots = startTimeBtwShots;  
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+        // fireDistance is exactly the camera's width in worldspace units multipled by the cameraDistanceMultiplier
+        fireDistance = 2.0f * Camera.main.orthographicSize * Camera.main.aspect * cameraDistanceMultiplier;
+    }
 
 
-	}
-
-	else {
-	    timeBtwShots -= Time.deltaTime; 
-	}
-
-	}
+    void Update()
+    {
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector2 playerVector = new Vector2(playerTransform.position.x - gameObject.transform.position.x, playerTransform.position.y - gameObject.transform.position.y);
+        Debug.Log("PLAYER DISTANCE: " + playerVector.magnitude);
+        Debug.Log("PLAYER VECTOR...");
+        Debug.Log(playerVector);
+        Debug.Log("FIRE DISTANCE: " + fireDistance);
+        Debug.DrawLine(gameObject.transform.position, enemyFirePoint.position, Color.white);
+        if (playerVector.magnitude <= fireDistance)
+        {
+            if (timeBtwShots <= 0)
+            {
+                Instantiate(projectile, enemyFirePoint.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
+        }
+        // timeBtwShots = timeBtwShots <= 0 ? startTimeBtwShots : timeBtwShots - Time.deltaTime;
+    }
 
 
 
