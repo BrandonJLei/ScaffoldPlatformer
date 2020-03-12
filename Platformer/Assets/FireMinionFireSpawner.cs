9f8c16c708d581011762695f647c36885ed40f81
Victoria W <56736494+Tori-W@users.noreply.github.com>
2020-03-11 21:15:24 -0700

    FireBoss - Changed respawn times
    - Enemy1 spawns at 5 seconds
    - Enemy2 spawns at 20 seconds
    - Enemies respawn after 7 seconds

---
+++ Platformer/Assets/FireMinionFireSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMinionFireSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy1;
+    public GameObject enemy2;
+
+    void Start()
+    {
+        enemy1.SetActive(false);
+        enemy2.SetActive(false);
+        Invoke("spawnEnemy", 5.0f);
+        Invoke("spawnEnemy2", 20.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnEnemy()
+    {
+        enemy1.SetActive(true);
+    }
+
+    void spawnEnemy2()
+    {
+        enemy2.SetActive(true);
+    }
+}
