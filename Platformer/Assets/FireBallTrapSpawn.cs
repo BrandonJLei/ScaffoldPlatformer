91add3581d0473fe847d559a3d031e73ecb9d763
damianrivas <drivas1993@gmail.com>
2020-03-11 21:07:31 -0700

    Finish Fireball trap

---
+++ Platformer/Assets/FireBallTrapSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallTrapSpawn : MonoBehaviour
+{
+    public float launchTick = 3.0f;
+    public GameObject fireball;
+    public Transform launchPoint;
+    private float tick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tick = launchTick;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log("TICK: " + tick);
+        if (tick <= 0)
+        {
+            Debug.Log("LAUNCHING");
+            Instantiate(fireball, launchPoint.position, launchPoint.rotation);
+            tick = launchTick;
+        }
+        else
+            tick -= Time.deltaTime;
+    }
+}
